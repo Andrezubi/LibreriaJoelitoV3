a0c2a9f5e1869ef653b109eee5292201cf1b5aa8
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2026-05-19 13:49:09 -0400

    configuracion strings de conexion a Mongo

--- MicroServicioProveedores/MicroServicioProveedores/Program.cs
+++ MicroServicioProveedores/MicroServicioProveedores/Program.cs
@@ -1,3 +1,5 @@
+using MicroservicioProveedores.Infraestructura.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+var bd = RepositorioBD.Instancia;
+
+var mongoSettings = builder.Configuration.GetSection("MongoDbSettings");
+
+var connectionString = mongoSettings["ConnectionString"];
+var databaseName = mongoSettings["DatabaseName"];
+
+if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(databaseName))
+{
+    throw new InvalidOperationException("La configuración de MongoDB (ConnectionString o DatabaseName) no está presente en appsettings.json");
+}
+
+bd.Initiate(connectionString, databaseName);
 
 // Configure the HTTP request pipeline.
 
