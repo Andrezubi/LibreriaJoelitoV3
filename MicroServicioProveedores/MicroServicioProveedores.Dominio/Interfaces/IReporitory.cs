be28dfa6bb4143a28071bcde6735a3d6114580af
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2026-05-19 12:49:35 -0400

    feat: creacion Singleton Mongo para Proveedores

---
+++ MicroServicioProveedores/MicroServicioProveedores.Dominio/Interfaces/IReporitory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServicioProveedores.Dominio.Interfaces
+{
+    internal interface IReporitory<T>
+    {
+        int Insertar(T t);
+        int Actualizar(T t);
+        int Eliminar(T t);
+        List<T> ObtenerTodo();
+    }
+}
