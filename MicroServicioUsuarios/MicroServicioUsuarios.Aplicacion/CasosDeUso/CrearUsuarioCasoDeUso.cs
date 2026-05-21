e826137d9a4de3581030ca215d0f2110293d7ad3
Zadropol <aaadrianrodriguezmontecinos@gmail.com>
2026-05-20 23:29:56 -0400

    completado aplicacion

---
+++ MicroServicioUsuarios/MicroServicioUsuarios.Aplicacion/CasosDeUso/CrearUsuarioCasoDeUso.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServicioUsuarios.Aplicacion.CasosDeUso
+{
+    internal class CrearUsuarioCasoDeUso
+    {
+    }
+}
---
+++ MicroServicioUsuarios/MicroServicioUsuarios.Aplicacion/InterfacesExt/IUsuarioFabrica.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServicioUsuarios.Aplicacion.InterfacesExt
+{
+    internal interface IUsuarioFabrica
+    {
+    }
+}
